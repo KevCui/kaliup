620f8df35ce5cb36cc9f4f1be6647f562f139e05
Kevin Cui <krazycavin@gmail.com>
2022-11-15 14:12:19 +0100

    Update apt.sh script

--- script/apt.sh
+++ script/apt.sh
@@ -5,22 +5,16 @@ run_sudo
 s="/etc/apt/sources.list"
 sd="/etc/apt/sources.list.d"
 sudo su -c 'echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" > '"$s"
-sudo su -c 'echo "deb http://deb.debian.org/debian buster main contrib non-free" >> '"$s"
-sudo su -c 'echo "deb-src http://deb.debian.org/debian buster main contrib non-free" >> '"$s"
-sudo su -c 'echo "deb http://security.debian.org/debian-security buster/updates main contrib" >> '"$s"
-sudo su -c 'echo "deb-src http://security.debian.org/debian-security buster/updates main contrib" >> '"$s"
+sudo su -c 'echo "deb http://deb.debian.org/debian bullseye main contrib non-free" >> '"$s"
+sudo su -c 'echo "deb-src http://deb.debian.org/debian bullseye main contrib non-free" >> '"$s"
+sudo su -c 'echo "deb http://security.debian.org/debian-security bullseye/updates main contrib" >> '"$s"
+sudo su -c 'echo "deb-src http://security.debian.org/debian-security bullseye/updates main contrib" >> '"$s"
 
 # unlock db
 sudo rm -f /var/lib/apt/lists/lock || true
 sudo rm -f /var/lib/dpkg/lock || true
 sudo rm -f /var/lib/dpkg/lock-frontend || true
 sudo rm -f /var/cache/apt/archives/lock || true
-
-# nodejs
-if [[ ! -f "$sd/nodesource.list" ]]; then
-    print_info "Adding nodejs source..."
-    sudo su -c "curl -sL https://deb.nodesource.com/setup_13.x | bash -"
-fi
 
 # yarn
 if [[ ! -f "$sd/yarn.list" ]]; then
